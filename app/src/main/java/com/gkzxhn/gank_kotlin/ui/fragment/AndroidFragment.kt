9b1dd6e1625635fbbcdd74d3de57387372cbe7c2
FangforFun <870431602@qq.com>
2017-06-22 12:00:39 +0800

    图片全屏页

--- app/src/main/java/com/gkzxhn/gank_kotlin/ui/fragment/AndroidFragment.kt
+++ app/src/main/java/com/gkzxhn/gank_kotlin/ui/fragment/AndroidFragment.kt
@@ -2,8 +2,12 @@ package com.gkzxhn.gank_kotlin.ui.fragment
 
 import android.databinding.DataBindingUtil
 import android.os.Bundle
+import android.support.v4.view.PagerAdapter
+import android.support.v4.view.ViewPager
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import com.bumptech.glide.Glide
 import com.gkzxhn.gank_kotlin.R
 import com.gkzxhn.gank_kotlin.databinding.FragmentAndroidBinding
@@ -13,6 +17,7 @@ import com.gkzxhn.gank_kotlin.mvp.presenter.GankPresenter
 import com.wingsofts.gankclient.bean.FuckGoods
 import com.wingsofts.gankclient.getMainComponent
 import kotlinx.android.synthetic.main.fragment_android.*
+import java.util.*
 import javax.inject.Inject
 
 /**
@@ -46,13 +51,44 @@ class AndroidFragment : GankContract.View, BaseFragment<FragmentAndroidBinding>(
     override fun setData(results: List<FuckGoods>, type: String) {
         when (type) {
             GirlFragment.GIRL -> {
-                Glide.with(iv.context).load(results[0].url)
-                        .crossFade()
-                        .into(iv)
-
+                val imageViewList = arrayListOf<ImageView>()
+                Collections.shuffle(results)
+                for (i in 0..4) {
+                    val imageView = ImageView(context)
+                    imageView.setScaleType(ImageView.ScaleType.CENTER)
+                    imageView.isClickable = true
+                    val url = results.get(i).url
+                    imageView.setOnClickListener { view ->
+                        ImageActivity.startActivity(context, imageView, url)
+                    }
+                    Glide.with(imageView.context)
+                            .load(url)
+                            .crossFade()
+                            .into(imageView)
+                    imageViewList.add(imageView)
+                }
+                viewpager.adapter = MyAdapter(imageViewList)
             }
             else -> {
             }
         }
+    }
+}
+
+class MyAdapter(private var results: ArrayList<ImageView>) : PagerAdapter() {
+
+    override fun isViewFromObject(view: View?, `object`: Any?): Boolean {
+        return view == `object`
+    }
+
+    override fun getCount() = results.size
+
+    override fun instantiateItem(container: ViewGroup?, position: Int): Any {
+        container!!.addView(results.get(position))
+        return results.get(position)
+    }
+
+    override fun destroyItem(container: ViewGroup?, position: Int, `object`: Any?) {
+        (container as ViewPager).removeView(results.get(position))
     }
 }
---
+++ app/src/main/java/com/gkzxhn/gank_kotlin/ui/fragment/ImageActivity.kt
@@ -0,0 +1,48 @@
+package com.gkzxhn.gank_kotlin.ui.fragment
+
+import android.app.Activity
+import android.app.ActivityOptions
+import android.content.Context
+import android.content.Intent
+import android.databinding.DataBindingUtil
+import android.os.Build
+import android.os.Bundle
+import android.util.Log
+import android.widget.ImageView
+import com.gkzxhn.gank_kotlin.R
+import com.gkzxhn.gank_kotlin.databinding.ActivityImageBinding
+import com.gkzxhn.gank_kotlin.ui.activity.BaseActivity
+import kotlinx.android.synthetic.main.activity_image.*
+
+/**
+ * Created by 方 on 2017/6/22.
+ */
+class ImageActivity : BaseActivity<ActivityImageBinding>() {
+    override fun initView() {
+        drag_photo.setOnExitListener { dragPhotoView, fl1, fl2, fl3, fl4 ->
+            Log.i("ImageActivity", "dragPhotoView 1 : " + fl1)
+            Log.i("ImageActivity", "dragPhotoView 1 : " + fl2)
+            Log.i("ImageActivity", "dragPhotoView 1 : " + fl3)
+            Log.i("ImageActivity", "dragPhotoView 1 : " + fl4)
+            finish()
+        }
+        mBinding.url = intent.getStringExtra(IMG)
+    }
+
+    override fun createDataBinding(savedInstanceState: Bundle?): ActivityImageBinding {
+        return DataBindingUtil.setContentView(this, R.layout.activity_image)
+    }
+
+    companion object{
+        val IMG = "IMG"
+        fun startActivity(context: Context, imageView: ImageView, url: String) {
+            val intent = Intent(context, ImageActivity::class.java)
+            intent.putExtra(IMG, url)
+            if(Build.VERSION.SDK_INT > 21) {
+                context.startActivity(intent, ActivityOptions.makeSceneTransitionAnimation(context as Activity, imageView, "img").toBundle())
+            }else{
+                context.startActivity(intent)
+            }
+        }
+    }
+}
