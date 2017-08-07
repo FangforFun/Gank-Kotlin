144dce17c46b055cfecb0f4a1464f590f63b26bb
FangforFun <870431602@qq.com>
2017-08-07 15:25:53 +0800

    加入版本更新部分...

--- app/src/main/java/com/gkzxhn/gank_kotlin/ui/activity/MainActivity.kt
+++ app/src/main/java/com/gkzxhn/gank_kotlin/ui/activity/MainActivity.kt
@@ -7,22 +7,24 @@ import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v4.app.ActivityCompat
 import android.support.v4.app.Fragment
+import android.util.Log
 import com.gkzxhn.gank_kotlin.R
 import com.gkzxhn.gank_kotlin.databinding.ActivityMainBinding
 import com.gkzxhn.gank_kotlin.ui.fragment.AndroidFragment
 import com.gkzxhn.gank_kotlin.ui.fragment.GirlFragment
 import com.gkzxhn.gank_kotlin.ui.fragment.ShipinFragment
 import com.gkzxhn.gank_kotlin.utils.PermissionsChecker
+import com.iflytek.autoupdate.IFlytekUpdate
+import com.iflytek.autoupdate.UpdateConstants
+import com.iflytek.autoupdate.UpdateErrorCode
+import com.iflytek.autoupdate.UpdateType
 import com.wingsofts.gankclient.toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 
 
-
-
-
-
 class MainActivity : BaseActivity<ActivityMainBinding>() {
+    val TAG = javaClass.simpleName
 
     // 所需的全部权限
     val PERMISSIONS = arrayOf(Manifest.permission.RECORD_AUDIO,
@@ -32,6 +34,8 @@ class MainActivity : BaseActivity<ActivityMainBinding>() {
     private lateinit var mPermissionsChecker: PermissionsChecker // 权限检测器
 
     override fun initView() {
+        updateVersion()
+
         mPermissionsChecker = PermissionsChecker(this);
         initFragments()
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
@@ -102,5 +106,33 @@ class MainActivity : BaseActivity<ActivityMainBinding>() {
             }
         }
     }
+
+    //初始化自动更新对象
+    val updManager = IFlytekUpdate.getInstance(this)
+
+    fun updateVersion(){
+        //开启调试模式，默认不开启
+//        updManager.setDebugMode(true)
+        //开启wifi环境下检测更新，仅对自动更新有效，强制更新则生效
+        updManager.setParameter(UpdateConstants.EXTRA_WIFIONLY, "true")
+        //设置通知栏使用应用icon，详情请见示例
+        updManager.setParameter(UpdateConstants.EXTRA_NOTI_ICON, "true")
+        //设置更新提示类型，默认为通知栏提示
+        updManager.setParameter(UpdateConstants.EXTRA_STYLE,UpdateConstants.UPDATE_UI_DIALOG)
+//        updManager.setParameter(UpdateConstants.EXTRA_STYLE, UpdateConstants.UPDATE_UI_NITIFICATION);
+        // 启动自动更新
+        updManager.autoUpdate(this, { errorCode, updateInfo ->
+            Log.i(TAG, "errorCode $errorCode")
+            Log.i(TAG, "updateInfo Url ${updateInfo.downloadUrl}")
+            Log.i(TAG, "updateInfo Version ${updateInfo.updateVersionCode}")
+            if(errorCode == UpdateErrorCode.OK && updateInfo!= null) {
+                if(updateInfo.getUpdateType() == UpdateType.NoNeed) {
+                    return@autoUpdate
+                }
+                updManager.showUpdateInfo(this, updateInfo);
+            }
+        })
+    }
+
 
 }
